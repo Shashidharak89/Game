ef362cc3d8ee3da2962a991dde90b3e849e58b38
Shashidhara K <shashidharak334@gmail.com>
2024-10-22 22:40:44 +0530

    Update About.js

--- src/component/About.js
+++ src/component/About.js
@@ -7,6 +7,7 @@ const About = () => {
       <h1>About Page</h1>
       <p>This is the About page!</p>
       <p>Hello world</p>
+    <iframe seamless="seamless" allowtransparency="true" allowfullscreen="true" frameborder="0" style="width: 100%;height: 100%;border: 0px;" src="https://zv1y2i8p.play.gamezop.com/g/Umfd7yvMd"> </iframe>
     </div>
   );
 };
