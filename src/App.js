efda02657a2c859337a3127b28ede8ef813420ca
Shashidharak89 <shashidharak334@gmail.com>
2024-10-24 11:37:31 +0530

    games added

--- src/App.js
+++ src/App.js
@@ -3,25 +3,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './component/Home';
 import About from './component/About';
+import './component/styles/style.css'
 
 const App = () => {
   return (
+    <div className='main'>
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className='link-items'>
+      <Link to="/" className='nav-items'>Home</Link>
+      <Link to="/about"  className='nav-items'>About</Link>
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </Router>
+    </div>
   );
 };
 
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,6 +1,12 @@
 // src/components/About.js
 import React from 'react';
 import './styles/home.css';  // Import CSS
+import myImage from './images/game1.jpg'; // Adjust the path if necessary
+import animal_connection from './images/animal connection.jpg';
+import dragon_annihilation from './images/dragon annihilation.jpg';
+import kingdom_fight from './images/kingdom fight 2.0.jpg';
+import slide_and_devide from './images/slide and devide.jpg';
+
 
 const Home = () => {
   return (
@@ -9,46 +15,156 @@ const Home = () => {
 
 
         <div className='game'>
+          <img src={myImage} alt="description" className='image-container' />
 
         </div>
         <div className='game'>
+          <img src={animal_connection} alt="description" className='image-container' />
 
         </div>
         <div className='game'>
+          <img src={dragon_annihilation} alt="description" className='image-container' />
 
         </div>
         <div className='game'>
+          <img src={kingdom_fight} alt="description" className='image-container' />
 
         </div>
+
         <div className='game'>
+          <img src={slide_and_devide} alt="description" className='image-container' />
 
         </div>
-        <div className='game'>
 
+        <div className='game'>
+          <img src={slide_and_devide} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={slide_and_devide} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+   <img src={kingdom_fight} alt="description" className='image-container' />
         </div>
         <div className='game'>
-
+          <img src={slide_and_devide} alt="description" className='image-container' />
         </div>
-        <div className='game'>
 
+        <div className='game'>
+          <img src={kingdom_fight} alt="description" className='image-container' />
         </div>
-        <div className='game'>
 
+        <div className='game'>
+          <img src={dragon_annihilation} alt="description" className='image-container' />
         </div>
-        <div className='game'>
 
+        <div className='game'>
+          <img src={dragon_annihilation} alt="description" className='image-container' />
         </div>
-        <div className='game'>
 
+        <div className='game'>
+          <img src={kingdom_fight} alt="description" className='image-container' />
         </div>
-        <div className='game'>
 
+        <div className='game'>
+          <img src={kingdom_fight} alt="description" className='image-container' />
         </div>
-        <div className='game'>
 
+        <div className='game'>
+          <img src={dragon_annihilation} alt="description" className='image-container' />
         </div>
-        <div className='game'>
 
+        <div className='game'>
+          <img src={kingdom_fight} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={kingdom_fight} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={kingdom_fight} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={kingdom_fight} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={kingdom_fight} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={kingdom_fight} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={dragon_annihilation} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={animal_connection} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={dragon_annihilation} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={animal_connection} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={dragon_annihilation} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={slide_and_devide} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={slide_and_devide} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={dragon_annihilation} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={animal_connection} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={slide_and_devide} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={animal_connection} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={kingdom_fight} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={dragon_annihilation} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={animal_connection} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={dragon_annihilation} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={animal_connection} alt="description" className='image-container' />
+        </div>
+
+        <div className='game'>
+          <img src={kingdom_fight} alt="description" className='image-container' />
         </div>
 
 
