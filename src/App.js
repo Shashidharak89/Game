c6bf943d50e15b4fc29e690f6b7b5fbfa111ae06
Shashidharak89 <shashidharak334@gmail.com>
2024-10-24 14:12:22 +0530

    added title and things

--- src/App.js
+++ src/App.js
@@ -8,16 +8,31 @@ import './component/styles/style.css'
 const App = () => {
   return (
     <div className='main'>
-    <Router>
-      <div className='link-items'>
-      <Link to="/" className='nav-items'>Home</Link>
-      <Link to="/about"  className='nav-items'>About</Link>
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+
+      <Router> 
+
+        <div className='dom-title'>
+
+          <div className='link-items-container'>
+            <label>GameNexPlay</label>
+          </div>
+
+          <div className='coins'>
+            <label>00000</label>
+          </div>
+        </div>
+        
+        <div className='link-items'>
+          <Link to="/" className='nav-items'>Home</Link>
+          <Link to="/about" className='nav-items'>About</Link>
+          <Link to="/about" className='nav-items'>Login/Signup</Link>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
