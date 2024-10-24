2b44ec2d25b4c03bd85f4ba6d34e44e00659e25d
Shashidharak89 <shashidharak334@gmail.com>
2024-10-24 22:17:19 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './component/Home';
 import About from './component/About';
+import Auth from './component/Auth';
 import './component/styles/style.css'
 import animal_connection from './component/images/coin.png';
 
@@ -10,29 +11,30 @@ const App = () => {
   return (
     <div className='main'>
 
-      <Router> 
+      <Router>
+        <div className='nav-bar'>
+          <div className='dom-title'>
 
-        <div className='dom-title'>
+            <div className='link-items-container'>
+              <label>GameNexPlay</label>
+            </div>
 
-          <div className='link-items-container'>
-            <label>GameNexPlay</label>
+            <div className='coins'>
+              <div className='logo'><img src={animal_connection} alt="coins" className='coin-img'></img></div>
+              <label>00000</label>
+            </div>
           </div>
 
-          <div className='coins'>
-            <div className='logo'><img src={animal_connection} alt="coins" className='coin-img'></img></div>
-            <label>00000</label>
+          <div className='link-items'>
+            <Link to="/" className='nav-items'>Home</Link>
+            <Link to="/about" className='nav-items'>About</Link>
+            <Link to="/Login" className='nav-items' id='login-signup'>Login/Signup</Link>
           </div>
         </div>
-        
-        <div className='link-items'>
-          <Link to="/" className='nav-items'>Home</Link>
-          <Link to="/about" className='nav-items'>About</Link>
-          <Link to="/about" className='nav-items'>Login/Signup</Link>
-        </div>
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/Login" element={<Auth />} />
         </Routes>
       </Router>
     </div>
